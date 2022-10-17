c41c7036c62eff124f05a2e80a9c9ad7ae880811
kjorolan <jorolank@gmail.com>
2022-10-17 12:14:23 +0800

    sample commit using vsc

--- src/common/customForm.js
+++ src/common/customForm.js
@@ -2,13 +2,11 @@ import React from "react";
 
 const CustomForm = ({ label, onChange, value, type, onClick, name }) => {
   return (
-    <>
     <form>
       <label>{label}</label>
       <input type={type} onChange={onChange} value={value} />
       <button onClick={onClick}>{name}</button>
     </form>
-    </>
   );
 };
 export default CustomForm;
