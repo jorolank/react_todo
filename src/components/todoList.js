e1ff4b9388c268c8f09e98912d8ea201f29a0a10
jorolank <58502197+jorolank@users.noreply.github.com>
2022-10-17 12:29:11 +0800

    added conditions

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -2,20 +2,24 @@ import React, { useContext } from "react";
 import { Context } from "./todoPage";
 
 const TodoList = () => {
-  const { todo, handleEdit, handleRemove, index : id } = useContext(Context);
+  const { todo, handleEdit, handleRemove, index: id } = useContext(Context);
   return (
     <ul>
-      {todo.map((item, index) => {
-        return (
-          <li key={index}>
-            {item}
-            {id !== index ? 
-            <button onClick={() => handleEdit(index)}>Update</button>
-            : ''}
-            <button onClick={() => handleRemove(index)}>Delete</button>
-          </li>
-        );
-      })}
+      {todo.length
+        ? todo.map((item, index) => {
+            return (
+              <li key={index}>
+                {item}
+                {id !== index ? (
+                  <button onClick={() => handleEdit(index)}>Update</button>
+                ) : (
+                  ""
+                )}
+                <button onClick={() => handleRemove(index)}>Delete</button>
+              </li>
+            );
+          })
+        : "No Todos"}
     </ul>
   );
 };
